301d5596980659017b23fc004c9e11bde560767f
Anuj Biyani <305254+anujbiyani@users.noreply.github.com>
2022-11-03 16:47:51 -0600

    add gallery with single image

--- pages/photos.js
+++ pages/photos.js
@@ -1,9 +1,24 @@
 import Layout from '../components/Layout';
+import styles from '../styles/Photos.module.css';
 
 export default function Photos() {
   return (
     <Layout>
       <h1>Photos</h1>
+
+      <div className={styles.gallery}>
+        <button>
+          <img src="assets/caret-left.svg" />
+        </button>
+
+        <div className={styles.imageContainer}>
+          <img src="https://media-cldnry.s-nbcnews.com/image/upload/rockcms/2021-12/christopher-lloyd-kb-main-211214-572486.jpg" />
+        </div>
+
+        <button>
+          <img src="assets/caret-right.svg" />
+        </button>
+      </div>
     </Layout>
   )
 }
