492bb91b1472081e2a22d0e0732bb9c13c0b0b59
Anuj Biyani <305254+anujbiyani@users.noreply.github.com>
2022-11-04 10:11:58 -0600

    hide nav buttons at gallery boundaries

--- pages/photos.js
+++ pages/photos.js
@@ -11,23 +11,30 @@ const images = [
 export default function Photos() {
   const [selectedImage, setSelectedImage] = useState(0);
 
+  const canNavigateBackwards = selectedImage > 0;
+  const canNavigateForwards = selectedImage < (images.length - 1);
+
   return (
     <Layout>
       <h1>Photos</h1>
 
       <div className={styles.gallery}>
-        <button onClick={() => setSelectedImage(selectedImage - 1)}>
-          <img src="assets/caret-left.svg" />
-        </button>
+        {canNavigateBackwards &&
+          <button onClick={() => setSelectedImage(selectedImage - 1)}>
+            <img src="assets/caret-left.svg" />
+          </button>
+        }
 
         <div className={styles.imageContainer}>
           <img src={images[selectedImage]} />
         </div>
 
-        <button onClick={() => setSelectedImage(selectedImage + 1)}>
-          <img src="assets/caret-right.svg" />
-        </button>
+        {canNavigateForwards &&
+          <button onClick={() => setSelectedImage(selectedImage + 1)}>
+            <img src="assets/caret-right.svg" />
+          </button>
+        }
       </div>
     </Layout>
-  )
+  );
 }
