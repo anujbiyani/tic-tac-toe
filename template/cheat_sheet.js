99a278c71bdd8758805b82885c3db8b71d519939
Anuj Biyani <305254+anujbiyani@users.noreply.github.com>
2022-12-09 12:39:20 -0700

    add cheat sheet

---
+++ template/cheat_sheet.js
@@ -0,0 +1,23 @@
+// Access element in array
+let array = [1, 2, 3, 4, 5];
+array[1] //=> 1
+
+// Assign element in array
+let array = [1, 2, 3, 4, 5];
+array[1] = 0 //=> [1, 0, 3, 4, 5]
+
+// Do something based on a condition
+if (1 === 1) {
+}
+
+// Create a function
+function nameOfFunction(argumentOne, argumentTwo) {
+  return "some value"
+}
+
+// Get an element from the DOM
+let element = document.getElementById('element-id');
+
+// Iterate through an array
+for (let index = 0; index < array.length; index++) {
+}
